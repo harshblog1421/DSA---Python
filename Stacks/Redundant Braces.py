4ae9a00a8b1cc87f0f14e0af870431e893090cc2
harshblog1421 <136053530+harshblog1421@users.noreply.github.com>
2025-02-11 18:19:36 +0530

    Create Redundant Braces.py

---
+++ Stacks/Redundant Braces.py
@@ -0,0 +1,60 @@
+Given a string A denoting an expression. It contains the following operators '+', '-', '*', '/'.
+Check whether A has redundant braces or not.
+NOTE: A will be always a valid expression and will not contain any white spaces.
+
+Problem Constraints
+1 <= |A| <= 105
+
+Input Format
+The only argument given is string A.
+
+Output Format
+Return 1 if A has redundant braces else, return 0.
+
+Example Input
+Input 1:
+ A = "((a+b))"
+
+Input 2:
+ A = "(a+(a+b))"
+
+Example Output
+Output 1:
+ 1
+
+Output 2:
+ 0
+
+Example Explanation
+Explanation 1:
+ ((a+b)) has redundant braces so answer will be 1.
+
+Explanation 2:
+(a+(a+b)) doesn't have have any redundant braces so answer will be 0.
+
+
+
+from collections import deque
+class Solution:
+    # @param A : string
+    # @return an integer
+    def braces(self, A):
+            st = deque()
+            for i in range(len(A)):
+
+                if A[i] in ('(','+','*','/','-'):
+                    st.append(A[i])
+
+                elif A[i] == ')':
+                    found_operator = False
+                    while len(st)>0 and st[-1] != '(':
+                        
+                        if st.pop() in ('+','-','*','/'):
+                            found_operator = True
+                    st.pop()
+
+                    if found_operator == False:
+                        return 1
+            
+            return 0
+  
